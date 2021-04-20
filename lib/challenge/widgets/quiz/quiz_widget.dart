33846566b5b3fa60ffdf683f415906c0d2b13253
Luis Moromisato <moromisato.luis@gmail.com>
2021-04-20 20:30:48 -0300

    created quiz widget

---
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -0,0 +1,36 @@
+import 'package:dev_quiz/challenge/widgets/answer/answer_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
+
+class QuizWidget extends StatelessWidget {
+  final String title;
+  const QuizWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.heading,
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          AnswerWidget(
+              isRight: true,
+              isSelected: true,
+              title:
+                  "Possibilita criação de aplicativos compilados nativamente"),
+          AnswerWidget(
+              title:
+                  "Possibilita criação de aplicativos compilados nativamente"),
+          AnswerWidget(
+              title:
+                  "Possibilita criação de aplicativos compilados nativamente")
+        ],
+      ),
+    );
+  }
+}
