6edb206c2b2f5e41d19300631b83791b803a9d0a
Luis Moromisato <moromisato.luis@gmail.com>
2021-04-21 18:29:36 -0300

    added dynamic properties

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,10 +1,13 @@
 import 'package:dev_quiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:dev_quiz/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:dev_quiz/core/app_gradients.dart';
 import 'package:dev_quiz/core/app_text_styles.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  final UserModel user;
+
+  AppBarWidget({required this.user})
       : super(
             preferredSize: Size.fromHeight(250),
             child: Container(
@@ -24,7 +27,7 @@ class AppBarWidget extends PreferredSize {
                             style: AppTextStyles.title,
                             children: [
                               TextSpan(
-                                  text: "Luís DEV",
+                                  text: user.name,
                                   style: AppTextStyles.titleBold)
                             ])),
                         Container(
@@ -33,8 +36,7 @@ class AppBarWidget extends PreferredSize {
                           decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(10),
                               image: DecorationImage(
-                                  image: NetworkImage(
-                                      "https://avatars.githubusercontent.com/u/12678874?s=400&u=5e283b544b886ab4985d1851bdae28da8e2aed44&v=4"))),
+                                  image: NetworkImage(user.photoUrl))),
                         )
                       ],
                     ),
